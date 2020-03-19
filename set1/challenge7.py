5758f04cfd6128d58b26b26f288c451567224847
kmille <github@androidloves.me>
2020-03-19 20:36:09 +0100

    add encrypt function for ecb and cbc

--- set1/challenge7.py
+++ set1/challenge7.py
@@ -9,11 +9,21 @@ def ecb_decrypt(cipher_text: bytes, key: bytes) -> bytes:
     message = decipher.decrypt(cipher_text)
     return message
 
+
+def ecb_encrypt(message: bytes, key: bytes) -> bytes:
+    assert len(key) % 16 == 0
+    crypter = AES.new(key, AES.MODE_ECB)
+    cipher = crypter.encrypt(message)
+    return cipher
+
+
 if __name__ == '__main__':
     cipher = b64decode(open("7.txt").read())
     key = b"YELLOW SUBMARINE"
     message = ecb_decrypt(cipher, key)
     print(message.decode())
     assert message[:33] == b"I'm back and I'm ringin' the bell"
+    assert cipher == ecb_encrypt(message, key)
+    print("done")
 
 # base64 -d 7.txt | openssl enc -aes-128-ecb -d -K $(echo -n "YELLOW SUBMARINE" | xxd -p)
