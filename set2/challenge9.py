5be8fbaad2d0442f484fbf48ac08b6aa889286d7
kmille <github@androidloves.me>
2020-03-19 21:00:10 +0100

    fix pkcs7 pading

--- set2/challenge9.py
+++ set2/challenge9.py
@@ -2,14 +2,14 @@
 
 
 def pkcs7_encode(message: bytes, block_size: int = 16) -> bytes:
-    remainder = block_size % len(message)
+    remainder = block_size - (len(message) % block_size)
     encoded_message = message + bytes(remainder * [remainder])
     return encoded_message
 
 
 if __name__ == '__main__':
     m = b"this is a test"
-    encoded_m = pkcs7_encode(m)
-    print(encoded_m)
-    assert encoded_m == b"this is a test\x02\x02"
+    encoded_message = pkcs7_encode(m)
+    print(encoded_message)
+    assert encoded_message == b"this is a test\x02\x02"
 
